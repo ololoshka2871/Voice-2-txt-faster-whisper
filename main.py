95e9a0e591e41718b7e828aae995d7a29d33c55d
Lolka servantov <Lolka@servant>
2023-04-19 13:14:45 +0300

    load models

--- main.py
+++ main.py
@@ -6,8 +6,25 @@ from aiohttp import web
 import functools
 import argparse
 
+from transformers import AutoModelForCTC, Wav2Vec2Processor, AutoModelForSeq2SeqLM, T5TokenizerFast
+
+import librosa
+import torch
+from safetensors.torch import load_file
+
 
 logger = logging.getLogger(__name__)
+
+
+def map_to_result(batch):
+    with torch.no_grad():
+        input_values = torch.tensor(
+            batch, device="cuda").unsqueeze(0)  # , device="cuda"
+        logits = model(input_values).logits
+
+    pred_ids = torch.argmax(logits, dim=-1)
+    batch = processor.batch_decode(pred_ids)[0]
+    return batch
 
 
 async def index(request: web.Request) -> web.Response:
@@ -15,60 +32,50 @@ async def index(request: web.Request) -> web.Response:
     return web.FileResponse('index.html')
 
 
-#async def say_get(tts: SileroTTS, request: web.Request) -> web.StreamResponse:
-#    params = request.rel_url.query
-#
-#    # get voice_id value
-#    voice_id = request.rel_url.query.get('voice_id', None)
-#
-#    # get text to say
-#    text = params.get('text', '')
-#
-#    logger.info(f'Generating audio ({voice_id}) for text: "{text}"')
-#
-#    # generate audio file
-#    data = tts.say(text, voice_id)
-#
-#    response = web.StreamResponse()
-#    response.headers['Content-Type'] = 'audio/wav'
-#
-#    writer = await response.prepare(request)
-#    await writer.write(data)
-#    await writer.drain()
-#
-#    return response
-#
-#
-#async def say_post(tts: SileroTTS, request: web.Request) -> web.StreamResponse:
-#    # get voice_id value
-#    voice_id = request.rel_url.query.get('voice_id', None)
-#
-#    # get text to say
-#    text = (await request.read()).decode('utf-8')
-#
-#    logger.info(f'Generating audio ({voice_id}) for text: "{text}"')
-#
-#    # generate audio file
-#    data = tts.say(text, voice_id)
-#
-#    response = web.StreamResponse()
-#    response.headers['Content-Type'] = 'audio/wav'
-#
-#    writer = await response.prepare(request)
-#    await writer.write(data)
-#    await writer.drain()
-#
-#    return response
-#
+async def recognize_post(models: dict, request: web.Request) -> web.StreamResponse:
+    # get voice_id value
+    # voice_id = request.rel_url.query.get('voice_id', None)
+
+    # get text to say
+    # text = (await request.read()).decode('utf-8')
+
+    # logger.info(f'Generating audio ({voice_id}) for text: "{text}"')
+
+    # generate audio file
+    # data = tts.say(text, voice_id)
+
+    # response = web.StreamResponse()
+    # response.headers['Content-Type'] = 'audio/wav'
+
+    # writer = await response.prepare(request)
+    # await writer.write(data)
+    # await writer.drain()
+
+    # return response
+    return None
+
 
 async def start_server() -> web.Application:
+    device = "cuda" if torch.cuda.is_available() else "cpu"
+
+    models = dict(
+        wav2vec2_model=AutoModelForCTC.from_pretrained(
+            "UrukHan/wav2vec2-russian", cache_dir="models").to(device),
+        wav2vec2_processor=Wav2Vec2Processor.from_pretrained(
+            "UrukHan/wav2vec2-russian", cache_dir="models", device=device),
+
+        t5_ru_spell_model=AutoModelForSeq2SeqLM.from_pretrained(
+            "UrukHan/t5-russian-spell", cache_dir="models").to(device),
+        t5_ru_spell_tokenizer=T5TokenizerFast.from_pretrained(
+            "UrukHan/t5-russian-spell", cache_dir="models", device=device),
+    )
+
     app = web.Application()
 
     # call handle_request with tts as first argument
     app.add_routes([
         web.get('/', handler=index),
-        #web.get('/say', handler=functools.partial(say_get, tts)),
-        #web.post('/say', handler=functools.partial(say_post, tts))
+        web.post('/recognize', handler=functools.partial(recognize_post, models))
     ])
     return app
 
--- models/download.md
+++
@@ -1,3 +0,0 @@
-# Please download model files an place here
-* https://huggingface.co/UrukHan/t5-russian-spell => UrukHant5-russian-spell.safetensors
-* https://huggingface.co/UrukHan/wav2vec2-russian => UrukHant5-wav2vec2-russian.safetensors
